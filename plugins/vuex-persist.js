ae9d227caee99c0bc9da69b1abedb7c81f7cc95a
Brian Norris <bnorris@marlinnetwork.com>
2020-02-11 09:27:12 -0600

    removing reducer option, forgot I wanted to save the site name

--- plugins/vuex-persist.js
+++ plugins/vuex-persist.js
@@ -4,7 +4,7 @@ export default ({ store }) => {
   window.onNuxtReady(() => {
     new VuexPersistence({
       /* your options */
-      reducer: state => ({ lists: state.lists })
+      // reducer: state => ({ lists: state.lists })
     }).plugin(store)
   })
 }
